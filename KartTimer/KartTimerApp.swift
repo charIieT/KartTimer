127faab2635a7e9dcc9ebe52c511a6c2ae728115
Charlie <chxrlietaylor@outlook.com>
2025-12-13 16:12:41 +0000

    Initial Commit

---
+++ KartTimer/KartTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KartTimerApp.swift
+//  KartTimer
+//
+//  Created by Charlie Taylor on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct KartTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
